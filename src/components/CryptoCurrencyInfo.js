1f73afda4639c9998e9c6c5041376d8c66567fc0
codeclassifiers <codeclassifiers@gmail.com>
2020-09-22 22:22:38 +0530

    boilerplate added

---
+++ src/components/CryptoCurrencyInfo.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+function CryptoCurrencyInfo() {
+    return(
+        <div></div>
+    )
+}
+
+export default CryptoCurrencyInfo;
