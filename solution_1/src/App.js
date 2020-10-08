4de3608a121fe568e8d3e3df3b1be1104aef0f3a
codeclassifiers <codeclassifiers@gmail.com>
2020-10-08 07:43:37 +0530

    Code refactored

---
+++ solution_1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import CryptoCurrencyInfo from './components/CryptoCurrencyInfo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CryptoCurrencyInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ solution_1/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="Header">
+            <nav className="navbar navbar-dark bg-dark">
+               <div className="w-100 text-light">
+                   CryptoCurrency Information App
+                </div>     
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
